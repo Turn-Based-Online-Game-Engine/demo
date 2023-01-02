96c3d0bbb24d6ffe3e761eaaff0de6d746050caa
kokadva <konstantine.dvalishvil@gmail.com>
2023-01-02 15:42:18 +0400

    generalize stuff

--- src/main.ts
+++ src/main.ts
@@ -1,63 +1,55 @@
-import { interval, Observable, reduce, scan, Subject } from "rxjs";
-import { Subscriber } from "../node_modules/rxjs/dist/types/index";
-import * as readline from "readline-sync";
-
-const gameState = {
-    board: [['', '', ''], ['', '', ''], ['', '', '']],
-    players: ['player1', 'player2'],
-    playerSigns: {
-        "player1": "X",
-        "player2": "O"
-    },
-    turn: 'player1'
-}
-
-const printOutState = (state: any) => {
-    console.log("Game state so far:")
-    console.log("Board =>")
-    state.board.forEach((x: any) => console.log(x));
-    console.log(`Player's turn: ${state.turn}`)
-    console.log()
-}
-
-class PlayerMove {
-    public x: number
-    public y: number
-
-    constructor(x: number, y: number){
-        this.x = x;
-        this.y = y;
-    }
-}
+import { GameEngine } from "./flowcontroller";
 
 
-const observable: Observable<PlayerMove> = new Observable((subscriber: any) => {
-    while (true){
-        const answer = readline.question("What's your move ? ");
-        let rawMove = answer.split(' ')
-        let playerMove = new PlayerMove(parseInt(rawMove[0]), parseInt(rawMove[1]))
-        subscriber.next(playerMove);
+class WhoGotHigher extends GameEngine {
+    
+    getAccoumulator() {
+        return (state: any, playerAction: any): any => {
+            state.playerNumbers[state.playerTurn] = playerAction;
+
+            if (state.playerTurn == 'pl2') {
+                const pl1Move = state.playerNumbers['pl1'];    
+                const pl2Move = state.playerNumbers['pl2'];    
+                if ((pl1Move as number) > (pl2Move as number)){
+                    state.scores['pl1'] += 1;
+                } else {
+                    state.scores['pl2'] += 1;
+                }
+                state.playerTurn = 'pl1';
+            } else {
+                state.playerTurn = 'pl2';
+            }
+            return state;
+        }
     }
 
-});
+    getChangedStateHandler() {
+        return console.log
+    }
 
-printOutState(gameState);
+    getInitialGameState() {
+        return {
+            players: ['pl1', 'pl2'],
+            playerTurn: 'pl1',
+            scores: {
+                'pl1': 0,
+                'pl2': 0
+            },
+            playerNumbers: {
+                'pl1': undefined,
+                'pl2': undefined
+            }
+        }
+    }
+    
+}
 
-observable.pipe(
-    scan((state: any, move: PlayerMove): any => {
-        let playerMove = (move as PlayerMove);
-        let playerId = state.turn;
-        let playerSign = state.playerSigns[playerId]
-        state.board[playerMove.x][playerMove.y] = playerSign
-        
-        let playerIndex: number = state.players.indexOf(playerId);
-        
-        let nextPlayerIndex = (playerIndex + 1) % state.players.length;
-        state.turn = state.players[nextPlayerIndex];
-        
-        return state;
-    }, gameState)
-).subscribe(((result: any) => {
-    printOutState(result);
-}));
+const game = new WhoGotHigher();
+const playerActionSubject = game.startEngine();
+
+
+setTimeout(() => {playerActionSubject.next(10);}, 1000)
+setTimeout(() => {playerActionSubject.next(20);}, 1000)
+setTimeout(() => {playerActionSubject.next(30);}, 1000)
+setTimeout(() => {playerActionSubject.next(20);}, 1000)
 
